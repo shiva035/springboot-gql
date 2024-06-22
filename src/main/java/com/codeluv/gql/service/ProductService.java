e6a0f999586e9a9effd9426c38dbf45a6c20a01e
shiv <shiva.yad15891@gmail.com>
2024-06-22 09:51:13 +0530

    docs

--- src/main/java/com/codeluv/gql/service/ProductService.java
+++ src/main/java/com/codeluv/gql/service/ProductService.java
@@ -12,16 +12,34 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-//    getProducts getProductsByCategory updateStock
 
+    /**
+     * Retrieves a list of all products.
+     *
+     * @return List of products
+     */
     public List<Product> getProducts(){
        return productRepository.findAll();
     }
 
+    /**
+     * Retrieves a list of products filtered by the specified category.
+     *
+     * @param category The category by which to filter products
+     * @return List of products matching the category
+     */
     public List<Product> getProductsByCategory(String category){
         return productRepository.findByCategory(category);
     }
 
+    /**
+     * Updates the stock quantity of a product with the given ID.
+     *
+     * @param id   The ID of the product to update
+     * @param qty  The new stock quantity
+     * @return The updated product
+     * @throws RuntimeException if the product with the specified ID is not found
+     */
     public Product updateStock(int id, int qty){
         Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("product not found with id " + id));
         product.setQty(qty);
